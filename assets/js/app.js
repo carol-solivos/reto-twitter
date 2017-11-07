5a8323ce4320bcbd3d05c7c14232d4a9065ed9b9
carol-solivos <carol.solivos@gmail.com>
2017-11-07 18:45:41 -0400

    version 0.0.2 agregando contador y deshabilitación del botón Twittear

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,11 @@
+// llamando al boton para ejecutar la función newComment;
+
 var boton = document.getElementById('btn');
 boton.onclick = newComment;
 
+//función que habilita el boton, guarda el comentario y lo muestra en un div nuevo;
 function newComment(){	
+	boton.disabled = true;
 	var comentarios = document.getElementById('comentario').value;
 	document.getElementById('comentario').value = '';
 
@@ -10,11 +14,13 @@ function newComment(){
 		return false;
 	}*/
 
+	//obteniendo y creando nodos;
 	var putComment = document.getElementById('putComment');
 	var placeComment = document.createElement('div');
 	var addComment = document.createElement('p');
 	var placeText = document.createTextNode(comentarios);
 
+	// agregando nodos hijos a nodos padres;
 	addComment.appendChild(placeText);
 	placeComment.appendChild(addComment);
 	putComment.appendChild(placeComment);
@@ -22,3 +28,15 @@ function newComment(){
 
 
 }
+
+
+function textCounter(field, cnt, maxlimit) {       
+	var cntfield = document.getElementById(cnt)   
+	if (field.value.length > maxlimit)
+    field.value = field.value.substring(0, maxlimit);
+	else
+    cntfield.value = maxlimit - field.value.length;
+	if (cntfield.value.length < 120){
+		boton.disabled = false;
+	}
+};
