3fba8cc8c8cb330ed19c4411efecab00c3e4af73
carol-solivos <carol.solivos@gmail.com>
2017-11-12 23:28:17 -0400

    version 0.0.4

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,15 @@
 var boton = document.getElementById('btn');
 boton.onclick = newComment;
 
+
 //función que habilita el boton, guarda el comentario y lo muestra en un div nuevo;
 function newComment(){	
 	boton.disabled = true;
 	var comentarios = document.getElementById('comentario').value;
 	document.getElementById('comentario').value = '';
-
-	/*if (comentarios.length == 0 || comentario == null) {
-		alert('Ingresa un comentario');
-		return false;
-	}*/
+	var counting = document.getElementById('contando');
+	counting.innerHTML = 140;
+	counting.style.color = '#0DA5BC';
 
 	//obteniendo y creando nodos;
 	var putComment = document.getElementById('putComment');
@@ -24,32 +23,39 @@ function newComment(){
 	addComment.appendChild(placeText);
 	placeComment.appendChild(addComment);
 	putComment.appendChild(placeComment);
-
-
-
-}
+};
 
 // funcion contador con alertas de color
-function textCounter(field, cnt, maxlimit) {       
-	var cntfield = document.getElementById(cnt);
-	cntfield.value = maxlimit - field.value.length;
-	if (cntfield.value.length < 140){
+
+var comment = document.getElementById('comentario');
+	comment.onkeyup = counter;
+
+function counter(){
+
+	var counting = document.getElementById('contando');
+	var comentarios = document.getElementById('comentario').value;
+	var commentLength = comentarios.length;
+
+	counting.innerHTML = 140 - commentLength;
+
+	comment.style.height = (commentLength + 80) +"px";
+	
+	if (comentarios.length < 140){
 		boton.disabled = false;
-		cntfield.style.color = '#0DA5BC';
+		counting.style.color = '#0DA5BC';
 	}
-	if (field.value.length >= 120 && field.value.length < 130 ) {
-		cntfield.style.color = 'purple';
+	if (comentarios.length >= 120 && comentarios.length < 130 ) {
+		counting.style.color = 'purple';
 	}
-	if (field.value.length >= 130) {
-		cntfield.style.color = 'red';
+	if (comentarios.length >= 130) {
+		counting.style.color = 'red';
 	}
-	if (field.value.length > 140) {
+	if (comentarios.length > 140) {
 		boton.disabled = true;
-		cntfield.style.color = 'red';
+		counting.style.color = 'red';
 	}
-	if (field.value.length == 0) {
+	if (comentarios.length == 0) {
 		boton.disabled = true;
 	}
 
-		
 };
